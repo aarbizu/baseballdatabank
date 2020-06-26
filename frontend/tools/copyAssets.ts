ef60a687344a71d3bb0d4b9101821a7a60e0699e
Alan Arbizu <alan.arbizu@gmail.com>
2020-06-26 12:13:44 -0700

    add public css for later styling

--- frontend/tools/copyAssets.ts
+++ frontend/tools/copyAssets.ts
@@ -1,3 +1,7 @@
 import * as shell from 'shelljs';
 
-shell.cp('-R', 'src/views', 'dist/');
+// Copy all the view templates and assets in the public folder
+shell.cp('-R', ['src/views', 'src/public'], 'dist/');
+
+// Remove unnecessary files
+shell.rm( ['dist/public/js/*.ts', 'dist/public/js/*.json'] );
